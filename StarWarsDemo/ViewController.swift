041afc934219e5248832b2277373ab3a080de885
Никита Королев <nickkorol1994@gmail.com>
2019-07-22 17:52:53 +0300

    Initial Commit

---
+++ StarWarsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsDemo
+//
+//  Created by Nikita Korolev on 22/07/2019.
+//  Copyright © 2019 Никита Королев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
